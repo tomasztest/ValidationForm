cb1b4a98a3e8b957d596527f2901a932390a64f7
tomasztest <tomaszkotzur@gmail.com>
2019-08-21 10:42:58 +0200

    Updates

---
+++ precache-manifest.ebc428d80dc116fa2259f817265771c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eec1fbc7d7c819c8ad85",
+    "url": "/ValidationForm/static/css/main.335d4708.chunk.css"
+  },
+  {
+    "revision": "eec1fbc7d7c819c8ad85",
+    "url": "/ValidationForm/static/js/main.eec1fbc7.chunk.js"
+  },
+  {
+    "revision": "d439e8744bee3815eb5d",
+    "url": "/ValidationForm/static/js/1.d439e874.chunk.js"
+  },
+  {
+    "revision": "59023eb3b776c0086b6a",
+    "url": "/ValidationForm/static/js/runtime~main.59023eb3.js"
+  },
+  {
+    "revision": "7688b844bd625fa519fe49a091668ea9",
+    "url": "/ValidationForm/index.html"
+  }
+];
